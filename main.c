3306f97aa70ca22510d5dd2cfe69894e65f0690c
angel¿ <155915830+NopAngel@users.noreply.github.com>
2026-06-11 17:37:36 -0400

    `improve main.c: comments`

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
+/*eMacs: <prj>*/
+
+/* fconverter: A CLI app that lets you convert text 
+ *  to binary, hexadecimal, and UTF-16BE
+ *
+ *  by: <NopAngel>/angel¿
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
+/* to_binary(): 
+ *  convert the string to to_binary
+ */
 void to_binary(const char* input) {
     printf("Binary: ");
     for (int i = 0; input[i] != '\0'; i++) {
@@ -13,6 +24,9 @@ void to_binary(const char* input) {
     printf("\n");
 }
 
+/* to_hex(): 
+ *  convert the string to hex
+ */
 void to_hex(const char* input) {
     printf("Hex:    ");
     for (int i = 0; input[i] != '\0'; i++) {
@@ -21,6 +35,9 @@ void to_hex(const char* input) {
     printf("\n");
 }
 
+/* to_utf16_hex(): 
+ *  convert the string to utf16_hex
+ */
 void to_utf16_hex(const char* input) {
     printf("UTF-16BE (Hex): ");
     for (int i = 0; input[i] != '\0'; i++) {
@@ -29,6 +46,9 @@ void to_utf16_hex(const char* input) {
     printf("\n");
 }
 
+/* main(): 
+ *  main function btw
+ */
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         printf("Usage: %s \"text to convert\"\n", argv[0]);
